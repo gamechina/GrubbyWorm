85f654e4bdc7303350da20356e1a399b512e6598
Wayne <Wayne@WaynedeMacBook-Pro.local>
2015-08-16 18:04:00 +0800

    add worm states

---
+++ GrubbyWorm/State-Machines/WormDefeatedState.swift
@@ -0,0 +1,13 @@
+//
+//  WormDefeatedState.swift
+//  GrubbyWorm
+//
+//  Created by Wayne on 15/8/16.
+//  Copyright © 2015年 GAME-CHINA.ORG. All rights reserved.
+//
+
+import UIKit
+
+class WormDefeatedState: WormState {
+
+}
---
+++ GrubbyWorm/State-Machines/WormHappyState.swift
@@ -0,0 +1,13 @@
+//
+//  WormHappyState.swift
+//  GrubbyWorm
+//
+//  Created by Wayne on 15/8/16.
+//  Copyright © 2015年 GAME-CHINA.ORG. All rights reserved.
+//
+
+import GameplayKit
+
+class WormHappyState: WormState {
+
+}
